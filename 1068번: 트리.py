ab97e0ffe099e9d453261f693e0c19b4f161ed57
juan0501 <55674737+juan0501@users.noreply.github.com>
2021-01-07 11:58:21 +0900

    Create 1068번: 트리.py

---
+++ 1068번: 트리.py
@@ -0,0 +1,28 @@
+n = int(input())
+parents = list(map(int, input().split()))
+del_node = int(input())
+
+tree = {} #dictionary 
+
+for i in range(n):
+    #self or child node
+    if i == del_node or parents[i] ==del_node:
+        continue
+    if parents[i] in tree:
+        tree[parents[i]].append(i)
+    else:
+        tree[parents[i]] = [i] #key(self.parent) = value(index)
+
+res = 0
+if -1 in tree:
+    que = [-1]
+else:
+    que = []
+while que:
+    node = que.pop()
+    if node not in tree:
+        res += 1
+    else:
+        que += tree[node]
+
+print(res)
