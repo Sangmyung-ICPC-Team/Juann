749a27f5e1c8ad22f45e9d7e84c9441ee187fc97
juan0501 <55674737+juan0501@users.noreply.github.com>
2021-03-04 18:08:49 +0900

    Gold 5

---
+++ BaekJoon/20026번: 적록색약.py
@@ -0,0 +1,43 @@
+import copy
+import sys
+sys.setrecursionlimit(10**5)
+
+def depth_first_search(color, x, y, num, before):
+    if x < 0 or x >= num or y < 0 or y >= num:
+        return False
+    if color[x][y] != 'X' and color[x][y] == before:
+        value = color[x][y]
+        color[x][y] = 'X'
+        depth_first_search(color, x + 1, y, num, value)
+        depth_first_search(color, x - 1, y, num, value)
+        depth_first_search(color, x, y + 1, num, value)
+        depth_first_search(color, x, y - 1, num, value)
+        return True
+    return False
+    
+num = int(input())
+color = [] #R, G, B
+
+for i in range(num):
+    color.append(list(input()))
+
+temp = copy.deepcopy(color) #deep copy
+cnt = 0
+
+for i in range(num):
+    for j in range(num):
+        if depth_first_search(color, i, j, num, color[i][j]) == True:
+            cnt += 1
+
+for i in range(num):
+    for j in range(num):
+        if temp[i][j] == 'G':
+            temp[i][j] = 'R'
+
+count = 0
+for i in range(num):
+    for j in range(num):
+        if depth_first_search(temp, i, j, num, temp[i][j]) == True:
+            count += 1
+print(cnt, count)
+
