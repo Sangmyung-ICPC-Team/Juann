bbcb3f49245d68dfc0a14407063a78275ae77279
juan0501 <55674737+juan0501@users.noreply.github.com>
2021-04-29 20:49:59 +0900

    Silver 3

---
+++ BaekJoon/2346번: 풍선 터뜨리기.py
@@ -0,0 +1,23 @@
+from sys import stdin
+from collections import deque
+
+if __name__ == "__main__":
+    N = int(stdin.readline())
+    q = deque(enumerate(map(int, stdin.readline().split())))
+    answer = []
+
+    while True:
+        idx, paper = q.popleft()
+        answer.append(idx + 1)
+
+        if not q:
+            break
+
+        if paper > 0:
+            # rotate(-) == append(q.popleft())
+            q.rotate(-(paper - 1))
+        elif paper < 0:
+            # rotate(+) == appendleft(q.pop())
+            q.rotate(-paper)
+
+    print(' '.join(map(str, answer)))
