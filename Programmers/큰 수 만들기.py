3c3bf43c88bf58fdad9c270c93708ed19b14eb6a
juan0501 <55674737+juan0501@users.noreply.github.com>
2021-05-20 16:05:19 +0900

    Level 2

---
+++ Programmers/큰 수 만들기.py
@@ -0,0 +1,18 @@
+def solution(number, k):
+    #__init__
+    stack = []
+    
+    for i, num in enumerate(number):
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+    
+        if k == 0:
+            stack += number[i:]
+            break
+        stack.append(num)
+    
+    stack = stack[:-k] if k > 0 else stack
+    answer = "".join(stack)
+    
+    return answer
